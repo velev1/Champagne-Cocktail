60cc58d243cc08685f8d50c40b379884c45f71a5
nivalen292 <nivalen292@outlook.com>
2017-03-17 18:55:38 +0200

    Show both containers if the player-statistics button is clicked

--- team-proj/scripts/players-statistics.js
+++ team-proj/scripts/players-statistics.js
@@ -1,12 +1,41 @@
 //Test
 var $options = $("#options-container");
 var $playerButton = $("#btnStatisticsPleyer");
-$playerButton.on("click", function(e) {
-    $options.css("background-color", "white")
-    .css("opacity", "0.5")
+var $charts = $("#charts-container");
+
+$options.css("background-color", "white") // Вместо .css ще е .addClass() след като има готов във css файла със същите стойности
+    .css("opacity", "0")
     .css("border-radius", "10px");
-    $options.hide();
-    $options.fadeIn();
+
+$charts.css("background-color", "white") // Вместо .css ще е .addClass() след като има готов във css файла със същите стойности
+    .css("opacity", "0")
+    .css("border-radius", "10px");
+
+//$options.hide();
+//$charts.hide();
+$playerButton.on("click", function (e) {
+    //$options.fadeToggle();
+    if ($charts.css("opacity") == 0.8) { //close
+        $charts.animate({
+            opacity: "0"
+        }, 500);
+    }
+    else { //open
+        $charts.animate({
+            opacity: "0.8"
+        }, 500);
+    }
+    if ($options.css("opacity") == 0.8) { //close
+        console.log($options.css("opacity"))
+        $options.animate({
+            opacity: "0"
+        }, 500);
+    }
+    else {
+        $options.animate({ //open
+            opacity: "0.8"
+        }, 500);
+    }
 });
 
 
