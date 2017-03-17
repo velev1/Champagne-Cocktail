105628d46d1b2b77b0bb1466154f60b8fecb5039
velev1 <velev_bg@mail.bg>
2017-03-17 11:24:54 +0200

    HTML structure added.

---
+++ team-proj/scripts/test.js
@@ -0,0 +1,11 @@
+
+var div = document.createElement("div");
+div.setAttribute("display","inline-block");
+var btn = document.createElement("input");
+btn.setAttribute("type","button");
+btn.className = "customButton";
+btn.setAttribute("value","some text");
+
+var el  = document.getElementById("options-container");
+div.appendChild(btn);
+el.appendChild(div);
