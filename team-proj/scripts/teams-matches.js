b238e987df81ab801b3b94c100f6236d4865ede0
velev1 <velev_bg@mail.bg>
2017-03-17 14:27:23 +0200

    Add data template

---
+++ team-proj/scripts/teams-matches.js
@@ -0,0 +1,18 @@
+var teamMatches = [
+    {
+        date:'',
+        teamHostName:'',
+        teamGuestName:'',
+        pointsHost:'',
+        pointsGuest:'',
+        attemptsHost:'',
+        attemptsguest:'',
+        treePointAttemptHost:'',
+        treePointAttemptGuest:'',
+        treePointMadeHost:'',
+        treePointGuestHost:'',
+        personalFoulsHost:'',
+        personalFoulsGest:'',
+        
+    }
+]
