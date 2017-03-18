7e2ff2fada674402c60194f0b06147d8399f3f45
krisi0505 <krisi0505@abv.bg>
2017-03-18 17:01:24 +0200

    ShowCompareOptions() onclick fix

--- team-proj/scripts/compare-players.js
+++
@@ -1,8 +0,0 @@
-function ShowCompareOptions(){
-	var aside = document.getElementById("options-container");
-	//Styling
-	aside.style.width = "400px";
-	aside.style.height = "400px";
-	aside.style.backgroundColor = "white";
-	aside.style.borderRadius = "12px";
-}
---
+++ team-proj/scripts/compare-teams.js
@@ -0,0 +1,8 @@
+function ShowCompareOptions(){
+	var aside = document.getElementById("options-container");
+	//Styling
+	aside.style.width = "400px";
+	aside.style.height = "400px";
+	aside.style.backgroundColor = "white";
+	aside.style.borderRadius = "12px";
+}
