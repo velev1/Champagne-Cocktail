c518fa66bd37405630da6f7da3b8e82ccc1cce05
velev1 <velev_bg@mail.bg>
2017-03-19 20:29:02 +0200

    test event teams-statistics.js

--- team-proj/scripts/teams-statistics.js
+++ team-proj/scripts/teams-statistics.js
@@ -1,9 +1,15 @@
 
 var btn = document.getElementById("btnStatisticsTeam");
- btn.addEventListener("click", function createOptionsTeamsStatistics(){
+btn.addEventListener("click", function createOptionsTeamsStatistics() {
     var el = document.getElementById("options-container");
+    console.log(el.firstChild);
+    while(el.firstChild) {
+        el.removeChild(el.firstChild);
+    }
+    
 
-
+    var conteiner = document.createElement("div");
+  
     var select = document.createElement("select");
     for (let i = 0; i < 10; i++) {
         let opt = document.createElement("option");
@@ -11,7 +17,8 @@ var btn = document.getElementById("btnStatisticsTeam");
         opt.innerHTML = 'team';
         select.appendChild(opt);
     }
-    el.appendChild(select);
+    container.appendChild(select);
+    el.appendChild(container);
     var teams = getTeamData();
     console.log(teams.length);
- });
+});
