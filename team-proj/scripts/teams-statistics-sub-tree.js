876cb89e2f82df4a8466153fbc7b285165c0440b
velev1 <velev_bg@mail.bg>
2017-03-19 18:03:11 +0200

    add team-data.js which stores data for the teams

--- team-proj/scripts/teams-statistics-sub-tree.js
+++ team-proj/scripts/teams-statistics-sub-tree.js
@@ -1,17 +1,5 @@
-var btn = document.getElementById("btnStatisticsTeam");
- btn.addEventListener("click", function createOptionsTeamsStatistics(){
-    var el = document.getElementById("options-container");
 
 
-    var select = document.createElement("select");
-    for (let i = 0; i < 10; i++) {
-        let opt = document.createElement("option");
-        opt.setAttribute("value", "team");
-        opt.innerHTML = 'team';
-        select.appendChild(opt);
-    }
-    el.appendChild(select);
- });
 
 
 
