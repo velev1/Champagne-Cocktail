486e1ea96eb560d87490bdcecc033ea3b9982647
velev1 <velev_bg@mail.bg>
2017-03-19 16:45:00 +0200

    Add event

--- team-proj/scripts/teams-statistics-sub-tree.js
+++ team-proj/scripts/teams-statistics-sub-tree.js
@@ -1,8 +1,17 @@
+var btn = document.getElementById("btnStatisticsTeam");
+ btn.addEventListener("click", function createOptionsTeamsStatistics(){
+    var el = document.getElementById("options-container");
+
+
+    var select = document.createElement("select");
+    for (let i = 0; i < 10; i++) {
+        let opt = document.createElement("option");
+        opt.setAttribute("value", "team");
+        opt.innerHTML = 'team';
+        select.appendChild(opt);
+    }
+    el.appendChild(select);
+ });
 
 
 
-
-function createOptionsTeamsStatistics() {
-
-
-}
