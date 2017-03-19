c63ea3d11337fedb61ccf0bb5d82808d190799d9
nivalen292 <nivalen292@outlook.com>
2017-03-19 17:52:51 +0200

    removed inline styles and made new files for the data

--- team-proj/scripts/compare-teams.js
+++
@@ -1,9 +0,0 @@
-function ShowCompareOptions(){
-	var aside = document.getElementById("options-container");
-	//Styling
-	aside.style.width = "400px";
-	aside.style.height = "400px";
-	aside.style.backgroundColor = "white";
-	aside.style.borderRadius = "12px";
-	aside.style.opacity=0.8;
-}
