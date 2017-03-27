c7a0ff94f0b636a59cb94adcc9259c73ef087c8b
krisi0505 <krisi0505@abv.bg>
2017-03-27 12:15:54 +0300

    Add warning messages to compare-teams

--- team-proj/scripts/compare-teams.js
+++ team-proj/scripts/compare-teams.js
@@ -8,32 +8,42 @@ btn.addEventListener("click", function createOptionsCompareTeams() {
     clearChart();
 
     var container = document.createElement("div");
-    container.setAttribute("class", "options-container");
+    container.setAttribute("class","options-container");
     var titleAside = document.createElement("h4");
     titleAside.innerHTML = "First team:";
     container.appendChild(titleAside);
-    var message = document.createElement("label");
-    message.innerHTML = "Please select a team.";
-    message.setAttribute("class", "hidden-message");
-    message.classList.add("warning");
 
-    message.setAttribute("id", "message-not-selected-team");
-    container.appendChild(message);
+    var messageFirstTeam = document.createElement("label");
+    messageFirstTeam.innerHTML = "Please select a team.";
+    messageFirstTeam.setAttribute("class", "hidden-message");
+    messageFirstTeam.classList.add("warning");
 
-    var div = document.createElement("div");
+    messageFirstTeam.setAttribute("id", "message-not-selected-first-team");
+    container.appendChild(messageFirstTeam);
+
+    var teams = teamData();
 
     var firstTeamSelect = createSelectTeamNames();
     firstTeamSelect.setAttribute("id", "first-team-select");
-    div.appendChild(firstTeamSelect);
-    container.appendChild(div);
-
+    container.appendChild(firstTeamSelect);
+    
     var secondTeamSelect = createSelectTeamNames();
     secondTeamSelect.setAttribute("id", "second-team-select");
-    div.appendChild(secondTeamSelect);
-    container.appendChild(div);
+    
+    var titleAside = document.createElement("h4");
+    titleAside.innerHTML = "Second team:";
+    container.appendChild(titleAside);
 
+    var messageSecondTeam = document.createElement("label");
+    messageSecondTeam.innerHTML = "Please select a team.";
+    messageSecondTeam.setAttribute("class", "hidden-message");
+    messageSecondTeam.classList.add("warning");
 
+    messageSecondTeam.setAttribute("id", "message-not-selected-second-team");
+    container.appendChild(messageSecondTeam);
 
+    container.appendChild(secondTeamSelect);
+    
     var btnCalculate = document.createElement("input");
     btnCalculate.setAttribute("type", "button");
     btnCalculate.setAttribute("class", "customButtonOptions");
@@ -49,62 +59,81 @@ btn.addEventListener("click", function createOptionsCompareTeams() {
     el.appendChild(container);
     btnCalculate.addEventListener("click", function calc() {
         const labelsTeams = ["two points accuracy", "three point accuracy", "free throws accuracy", "win rate"];
-        
-        var firstTeamName = $(firstTeamSelect).find(":selected").text();
-        var secondTeamName = $(secondTeamSelect).find(":selected").text();
 
-        var firstTeamId = "first-team-select";
-        var secondTeamId = "second-team-select";
-
-        let firstTeamWin = calculateTeamStatistic.winRate(firstTeamId);
-        let secondTeamWin = calculateTeamStatistic.winRate(secondTeamId);
-        let statisticValues = calculateTeamStatistic.shotAccuracy(firstTeamId);
-        let statisticValues2 = calculateTeamStatistic.shotAccuracy(secondTeamId);
-        statisticValues.push(firstTeamWin);
-        statisticValues2.push(secondTeamWin);
+        let firstSelection = firstTeamSelect.options[firstTeamSelect.selectedIndex].value;
+        let secondSelection = secondTeamSelect.options[secondTeamSelect.selectedIndex].value;
         
-        var draw = getCharts;
-        var drawObj = {
-            type: 'bar',
-            data: {
-                labels: labelsTeams,
-                datasets: [
-                    {
-                        label: firstTeamName,
-                        backgroundColor: 'rgba(255, 99, 132, 0.2)',
-                        borderColor: 'rgba(255,99,132,1)',
-                        data: statisticValues,
-                        borderWidth: 1
-                    },
-                    {
-                        label: secondTeamName,
-                        backgroundColor: 'rgba(54, 162, 235, 0.2)',
-                        borderColor: 'rgba(54, 162, 235, 1)',
-                        data: statisticValues2,
-                        borderWidth: 1
-                    }
-                ]
-            },
-            options: {
-                legend: {
-                    display: false 
-                },
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            max: 100, 
-                            min: 0, 
-                            beginAtZero: true
-                        }
-                    }]
-                }
+
+        if (firstSelection === "not selected") {
+            messageFirstTeam.classList.remove("hidden-message");
+        }else if(secondSelection === "not selected") {
+            messageSecondTeam.classList.remove("hidden-message");
+        }else{
+
+            if (messageFirstTeam.classList.length === 1) {
+                messageFirstTeam.classList.add("hidden-message")
             }
-        };
 
-        //clear and draw
-        clearChart();
-        var ctx = document.getElementById("myChart");
-        var myChart = new Chart(ctx, drawObj);
+            if (messageSecondTeam.classList.length === 1) {
+                messageSecondTeam.classList.add("hidden-message")
+            }
+
+            var firstTeamName = $(firstTeamSelect).find(":selected").text();
+            var secondTeamName = $(secondTeamSelect).find(":selected").text();
+
+            var firstTeamId = "first-team-select";
+            var secondTeamId = "second-team-select";
+
+            let firstTeamWin = calculateTeamStatistic.winRate(firstTeamId);
+            let secondTeamWin = calculateTeamStatistic.winRate(secondTeamId);
+            let statisticValues = calculateTeamStatistic.shotAccuracy(firstTeamId);
+            let statisticValues2 = calculateTeamStatistic.shotAccuracy(secondTeamId);
+            statisticValues.push(firstTeamWin);
+            statisticValues2.push(secondTeamWin);
+
+            var draw = getCharts;
+            var drawObj = {
+                type: 'bar',
+                data: {
+                    labels: labelsTeams,
+                    datasets: [
+                        {
+                            label: firstTeamName,
+                            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                            borderColor: 'rgba(255,99,132,1)',
+                            data: statisticValues,
+                            borderWidth: 1
+                        },
+                        {
+                            label: secondTeamName,
+                            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                            borderColor: 'rgba(54, 162, 235, 1)',
+                            data: statisticValues2,
+                            borderWidth: 1
+                        }
+                    ]
+                },
+                options: {
+                    legend: {
+                        display: false 
+                    },
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                max: 100, 
+                                min: 0, 
+                                beginAtZero: true
+                            }
+                        }]
+                    }
+                }
+            };
+
+            //clear and draw
+            clearChart();
+            var ctx = document.getElementById("myChart");
+            var myChart = new Chart(ctx, drawObj);
+        }
     });
 });
 
